e63b1ff0b689690a56006254ee03216b2c8def5c
khanhdang12 <90669438+khanhdang12@users.noreply.github.com>
2023-05-18 15:18:01 +0700

    Delete Prelab2 directory

--- Prelab2/2.Pointer1/1.cpp
+++
@@ -1,4 +0,0 @@
-int getValueAtIndex(int *ptr, int k)
-{
-    return ptr[k];
-}
--- Prelab2/4.Struct/1.cpp
+++
@@ -1,9 +0,0 @@
-struct SCP 
-{
-    int id;
-    int objClass;
-    string speConProcedures;
-    string description;
-    string *addendums;
-    int numAddendums;
-};
